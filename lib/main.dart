f767966924a7c3ee8aa25ebe6f18ce1db3790382
MohamedSaifAldawla <mohamedsaif12@outlook.com>
2022-12-11 23:24:46 +0300

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ void main() async {
   await init();
   GetThemeMode();
 
-  //------------- Firebase ---------------//
+  //------------- Firebase2 ---------------//
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
